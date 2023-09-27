bafd00e72b45e16d8ba7b5d0b260ece3b08e1b5c
almayawn <alma.laras@ui.ac.id>
2023-09-27 09:25:29 +0700

    Selesai Tugas 4

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
 from main.views import show_main, create_entry, show_xml, show_json, show_xml_by_id, show_json_by_id 
+from main.views import register, login_user, logout_user
+from main.views import increment_flower_amount, decrement_flower_amount, delete_flower
 
 app_name = 'main'
 
 urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
     path('', show_main, name='show_main'),
     path('create-entry', create_entry, name='create_entry'),
+    path('int-flower/<int:pk_id>/', increment_flower_amount, name='inc_flower'),
+    path('dec-flower/<int:pk_id>/', decrement_flower_amount, name='dec_flower'),
+    path('del-flower/<int:pk_id>/', delete_flower, name='del_flower'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'), 
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
--- main/views.py
+++ main/views.py
@@ -1,3 +1,10 @@
+import datetime
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from django.http import HttpResponse
 from django.core import serializers
 from django.http import HttpResponseRedirect
@@ -7,8 +14,53 @@ from django.shortcuts import render
 from main.models import Flower
 from django.contrib import messages
 
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {
+        'form':form,
+        'owner_name': 'Alma Laras Bestari',
+        'npm': '2206082303',
+        'class': 'PBP E',
+        'application': 'Memory Bouquet',
+        }
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main")) 
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+        else:
+            messages.info(request, 'Sorry, incorrect username or password. Please try again.')
+    context = {
+        'owner_name': 'Alma Laras Bestari',
+        'npm': '2206082303',
+        'class': 'PBP E',
+        'application': 'Memory Bouquet',
+    }
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
+
+@login_required(login_url='/login')
 def show_main(request):
-    flowers = Flower.objects.all()
+    flowers = Flower.objects.filter(user=request.user)
 
     total_entries = len(flowers)
     if (total_entries > 1):
@@ -17,12 +69,14 @@ def show_main(request):
         entry_messages = f'You have a total of {total_entries} entry'
 
     context = {
-        'name': 'Alma Laras Bestari',
+        'owner_name': 'Alma Laras Bestari',
         'npm': '2206082303',
         'class': 'PBP E',
         'application': 'Memory Bouquet',
+        'name': request.user.username,
         'flowers': flowers,
         'total_entry_message': entry_messages,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -31,13 +85,45 @@ def create_entry(request):
     form = FlowerForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        entry = form.save(commit=False)
+        entry.user = request.user
+        entry.save()
         name = form.cleaned_data['name']
         messages.success(request, f'succesfully added a new entry "{name}"')
         return HttpResponseRedirect(reverse('main:show_main'))
 
-    context = {'form': form}
+    context = {
+        'form': form,
+        'owner_name': 'Alma Laras Bestari',
+        'npm': '2206082303',
+        'class': 'PBP E',
+        'application': 'Memory Bouquet',
+    }
     return render(request, "create_entry.html", context)
+
+def increment_flower_amount(request, pk_id):
+    if request.method == 'POST':
+        flower = Flower.objects.get(id=pk_id)
+        flower.amount += 1
+        flower.save()
+    
+    return redirect('main:show_main')
+
+def decrement_flower_amount(request, pk_id):
+    if request.method == 'POST':
+        flower = Flower.objects.get(id=pk_id)
+        if flower.amount > 0:
+            flower.amount -= 1
+            flower.save()
+    
+    return redirect('main:show_main')
+
+def delete_flower(request, pk_id):
+    if request.method == 'POST':
+        flower = Flower.objects.get(id=pk_id)
+        flower.delete()
+    
+    return redirect('main:show_main')
 
 def show_xml(request):
     data = Flower.objects.all()
